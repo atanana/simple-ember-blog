3cfd32827edd757e49d7cb0d1d8385239bdad3bd
Andrei Tanana <tanana@email.ru>
2016-02-17 23:53:46 +0300

    display posts init

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      posts: [
+        {
+          title: 'test title',
+          text: 'test text'
+        },
+        {
+          title: 'test title 2',
+          text: 'test text 2'
+        },
+        {
+          title: 'test title 3',
+          text: 'test text 3'
+        }
+      ]
+    };
+  }
 });
