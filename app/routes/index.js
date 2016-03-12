20ad75863508e4c2e07341f4fcc80c0a0ae0d073
Andrei Tanana <tanana@email.ru>
2016-03-12 12:47:59 +0300

    change index model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {
-      posts: this.store.findAll('post')
-    };
+    return this.store.findAll('post');
   }
 });
