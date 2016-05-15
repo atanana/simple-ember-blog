7e1889648531dc9629bd18668590e3f04e420d83
atanana <tanana@email.ru>
2016-05-15 16:55:36 +0300

    login route test

--- tests/unit/routes/login-test.js
+++ tests/unit/routes/login-test.js
@@ -1,4 +1,7 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+
+const {RSVP} = Ember;
 
 moduleFor('route:login', 'Unit | Route | login', {
   // Specify the other units that are required for this test.
@@ -8,4 +11,38 @@ moduleFor('route:login', 'Unit | Route | login', {
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('it authenticates successfully', function (assert) {
+  assert.expect(2);
+  let route = this.subject();
+  route.set('session', {
+    authenticate(authenticator, data) {
+      assert.equal('authenticator:blog', authenticator);
+      assert.deepEqual({
+        login: 'login',
+        password: 'password'
+      }, data);
+      return RSVP.resolve();
+    }
+  });
+  route.send('authenticate', 'login', 'password');
+});
+
+test('it authenticates unsuccessfully', function (assert) {
+  assert.expect(2);
+  let route = this.subject();
+  let error = 'error';
+  route.set('session', {
+    authenticate() {
+        return RSVP.reject({error: error});
+    }
+  });
+  route.controller = {
+    set(property, value) {
+      assert.equal('errorMessage', property);
+      assert.equal(error, value);
+    }
+  };
+  route.send('authenticate');
 });
